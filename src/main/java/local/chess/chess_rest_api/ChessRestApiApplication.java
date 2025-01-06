1c02f4ee58d67ac55f35e992a0d23f06268fbd48
maik123100 <wesselkock.maik@gmail.com>
2025-01-06 12:07:48 +0100

    init java rest commit

---
+++ src/main/java/local/chess/chess_rest_api/ChessRestApiApplication.java
@@ -0,0 +1,13 @@
+package local.chess.chess_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessRestApiApplication.class, args);
+	}
+
+}
