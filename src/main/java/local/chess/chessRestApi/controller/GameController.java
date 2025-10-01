8fec6873032ab8bdf1396ed3f2062a7db72b0a40
Maik <maik.wesselkock@rwth-aachen.de>
2025-10-01 22:16:16 +0200

    + Cross Origin for Controller

--- src/main/java/local/chess/chessRestApi/controller/GameController.java
+++ src/main/java/local/chess/chessRestApi/controller/GameController.java
@@ -12,10 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import local.chess.chessRestApi.service.GameService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/games")
 public class GameController {
 
--- src/main/java/local/chess/chessRestApi/controller/StatusController.java
+++ src/main/java/local/chess/chessRestApi/controller/StatusController.java
@@ -3,9 +3,11 @@ package local.chess.chessRestApi.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1")
 public class StatusController {
 
